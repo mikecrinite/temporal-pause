c8564899f7a8cdaa63f55a44f6c14cddc6b28cd1
Michael Crinite <mikecrinite@gmail.com>
2025-07-29 22:36:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sample project
+
+Start the temporal server
+
+The Temporal Service will be available on localhost:7233.
+The Temporal Web UI will be available at http://localhost:8233.
---
+++ start_temporal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Windows
+# cd C:\Users\mikec;./temporal.exe server start-dev
+
+# Anywhere else
+temporal server start-dev
+
+# Add --db-filename your_temporal.db to use a database file
